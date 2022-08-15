94dbddedadf5bc01fb7faf2639d40f39a19ffb51
unknown <vitomartinsalmeida@gmail.com>
2022-08-15 14:25:22 -0300

    removendo rota home

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,6 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-/*
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-    ->name('home')
-    ->middleware('verified');
-*/
-
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
     ->middleware('verified');;
 
