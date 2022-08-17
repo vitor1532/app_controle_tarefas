ece3c0bf03251616a2c7ef3365d2835639b7770b
unknown <vitomartinsalmeida@gmail.com>
2022-08-17 16:30:15 -0300

    aplicando títulos nas tabelas de exportação

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,10 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return Tarefa::where('user_id', '=', auth()->user()->id)->get();
+    }
+
+    public function headings() :array
+    {
+        return ['ID Tarefa', 'ID Usuário', 'Tarefa', 'Data limite conclusão', 'Data criação', 'Data atualização'];
     }
 }
