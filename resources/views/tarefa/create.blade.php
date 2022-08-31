696057a4ac69537abdfe62056a2e24be6f4c56b6
Vitor <vitomartinsalmeida@gmail.com>
2022-08-31 14:32:42 -0300

    aplicando pequenos reparos

--- resources/views/tarefa/create.blade.php
+++ resources/views/tarefa/create.blade.php
@@ -9,21 +9,17 @@
                         <div class="mr-auto">Adicionar Tarefa</div>
                         <a href="{{ url()->previous() }}" class="btn btn-sm btn-primary">Voltar</a>
                     </div>
-                    {{--<div class="card-header" style="display: inline-flex">
-                        <div class="mr-auto">Tarefas</div>
-                        <a href="{{ route('tarefa.create') }}" class="btn btn-sm btn-info">Nova Tarefa</a>
-                    </div>--}}
                     <div class="card-body">
                         <form method="post" action="{{ route('tarefa.store') }}">
                             @csrf
                             <div class="mb-3">
                                 <label class="form-label">Tarefa</label>
-                                <input type="text" class="form-control" name="tarefa">
+                                <input type="text" class="form-control" name="tarefa" value="{{ old('tarefa') ?? '' }}">
                                 <div style="color: red;">{{ $errors->has('tarefa') ? $errors->first('tarefa') : '' }}</div>
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Data limite</label>
-                                <input type="date" class="form-control" name="data_limite_conclusao">
+                                <input type="date" class="form-control" name="data_limite_conclusao" value="{{ old('data_limite_conclusao') ?? '' }}">
                                 <div style="color: red;">{{ $errors->has('data_limite_conclusao') ? $errors->first('data_limite_conclusao') : '' }}</div>
                             </div>
                             <button type="submit" class="btn btn-primary">Cadastrar</button>
